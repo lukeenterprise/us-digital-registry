e4e1f8798a90a17fe5f41b2dec7f95b6007a8c9a
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-03-18 18:31:33 -0400

    updating devise session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,11 @@
-class User::SessionsController < Devise::SessionsController
+class SessionsController < Devise::SessionsController
     def destroy
         current_user.invalidate_session!
         super
     end
+    def logout
+        reset_session
+        render json:{ status:200, logged_out: true}
+    end
+
 end
