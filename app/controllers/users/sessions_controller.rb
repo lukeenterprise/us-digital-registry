1efe46d1be2f3021f142f05d69a2b2b9f10f2b4b
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-03-18 18:03:09 -0400

    replace users with user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Users::SessionsController < Devise::SessionsController
+class User::SessionsController < Devise::SessionsController
     def destroy
         current_user.invalidate_session!
         super
