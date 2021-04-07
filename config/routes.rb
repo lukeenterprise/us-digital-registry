d052816659ba77bf3fb1ecaecf9844b809566170
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-04-07 16:38:30 -0400

    revising changes

--- config/routes.rb
+++ config/routes.rb
@@ -332,7 +332,7 @@ Ringsail::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 	devise_scope :user do
-		get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+		get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
 	end
 
 
