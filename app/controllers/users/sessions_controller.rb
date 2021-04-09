836d04f452c58c373f5522ad751dfca3f173b0f9
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-04-09 14:37:21 -0400

    updating redirect url

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,7 @@
             Rails.logger.info session[:id_token]
             Rails.logger.info 'logging session end'
             logout_request = self.class.logout_utility.build_request(id_token: session[:id_token],
-              post_logout_redirect_uri: 'https://www.google.com'
+              post_logout_redirect_uri: root_path
             )
             sign_out(current_user)
             redirect_to(logout_request.redirect_uri) and return
