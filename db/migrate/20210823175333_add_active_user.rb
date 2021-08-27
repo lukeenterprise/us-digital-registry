ccb97d523771b5d25a60feb781d28de80ec3079a
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-08-27 08:31:22 -0400

    updated file name

--- db/migrate/20210823175333_add_active_user.rb
+++ db/migrate/20210823175333_add_active_user.rb
@@ -1,6 +1,6 @@
 class AddActiveUser< ActiveRecord::Migration
     def change   
-      add_column :users, :active, :boolean, :default => true
+      add_column :users, :isactive, :boolean, :default => true
       add_column :users, :last_activated_at, :datetime
     end
 end
