10405985c81cb81e31f98abb797c1f9a9927be77
krithika sreenivasan <krithika.sreenivasan@gsa.gov>
2021-08-29 18:12:22 -0400

    migration version updated

--- db/migrate/20210823175333_add_active_user.rb
+++ db/migrate/20210823175333_add_active_user.rb
@@ -1,4 +1,4 @@
-class AddActiveUser< ActiveRecord::Migration
+class AddActiveUser < ActiveRecord::Migration[5.2]
     def change   
       add_column :users, :isactive, :boolean, :default => true
       add_column :users, :last_activated_at, :datetime
