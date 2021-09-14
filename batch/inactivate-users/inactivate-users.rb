4984f522f0a0d53478757fb149e16828b41ad464
Socrates Geetha <socrates.geetha@gmail.com>
2021-09-13 21:02:34 -0400

    Updated to list all the environment variable names and print the count of users table

--- batch/inactivate-users/inactivate-users.rb
+++ batch/inactivate-users/inactivate-users.rb
@@ -8,7 +8,16 @@ logger.info('Description: Inactivate users who have not logged into the system i
 
 logger.debug('Program begin')
 
+logger.debug('Listing all environment keys')
+logger.debug(ENV.keys)
+
 logger.debug('Querying database to find all users who have not logged into the system for last 90 days')
+
+results = ActiveRecord::Base.connection.execute("select count(1) from users")
+
+logger.debug('Count of records in users')
+logger.debug(results)
+
 logger.debug('Update user to set active to false')
 
 logger.debug('Program Completed')
