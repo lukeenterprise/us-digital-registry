6919801dab5540daf91a729a8a98e35e1b3a8ddf
Socrates Geetha <socrates.geetha@gmail.com>
2021-09-13 20:57:57 -0400

    Updated to show the count of records in users table

--- batch/inactivate-users/inactivate-users.rb
+++ batch/inactivate-users/inactivate-users.rb
@@ -12,6 +12,12 @@ logger.debug('Listing all environment keys')
 logger.debug(ENV.keys)
 
 logger.debug('Querying database to find all users who have not logged into the system for last 90 days')
+
+results = ActiveRecord::Base.connection.execute("select count(1) from users")
+
+logger.debug('Count of records in users')
+logger.debug(results)
+
 logger.debug('Update user to set active to false')
 
 logger.debug('Program Completed')
